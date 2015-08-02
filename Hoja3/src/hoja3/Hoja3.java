5b186830b632f362bc1251888390785d72c798a5
Belen <mariabelenhb@gmail.com>
2015-08-01 18:15:11 -0600

    Intento de lectura de archivo .-.

--- Hoja3/src/hoja3/Hoja3.java
+++ Hoja3/src/hoja3/Hoja3.java
@@ -5,6 +5,8 @@
  */
 package hoja3;
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Ma. Belen
@@ -18,7 +20,7 @@ public class Hoja3 {
         // TODO code application logic here
         //Se crea un fichero para guardar 3000 numeros aleatorios
         File numeros= new File("numeros.txt");
-        //si ya existia, debe borrarse su contenido para sobreescribirlo
+        //si ya existia el archivo, debe borrarse su contenido para sobreescribirlo
         numeros.delete();
         Sorting sorting = new Sorting();
         //este ciclo llena el archivo de texto
@@ -26,6 +28,7 @@ public class Hoja3 {
             //intenta editarse el archivo
             try{
                 FileWriter editar = new FileWriter(numeros,true);
+                //se utiliza el metodo para generar numeros aleatorios de la clase Sorting
                 editar.write(sorting.numerosRandom()+" ");
                 editar.close();
             }
@@ -33,9 +36,36 @@ public class Hoja3 {
                 System.out.println ("Error al escribir");
             }
         }
+        //REVISAR LECTURA DE ARCHIVO PARA GUARDAR EN UN ARREGLO
+        //se procede a extraer los numeros del archivo de texto
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(numeros));
+                try {
+                    for(String line; (line = br.readLine()) != null; ){
+                        sorting.setString(line);
+                    }
+                }
+                catch(IOException ex) {
+                        Logger.getLogger(Hoja3.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        catch(FileNotFoundException ex) {
+            Logger.getLogger(Hoja3.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+        //probando si se guardo el arreglo
+        for (int x=0; x<3000;x++){
+            System.out.println(sorting.getListaSelection(x));
+        }
+        
+        //se llama dos veces a cada metodo, para medir el tiempo que cada sort tarda en ordenar y en recorrer al estar ya ordenado
         /*sorting.selectionSort();
+        sorting.selectionSort();
+        sorting.insertionSort();
         sorting.insertionSort();
         sorting.quickSort();
+        sorting.quickSort();
+        sorting.radixSort();
         sorting.radixSort();*/
     }
     
--- Hoja3/src/hoja3/Sorting.java
+++ Hoja3/src/hoja3/Sorting.java
@@ -10,12 +10,50 @@ package hoja3;
  * @author Ma. Belen
  */
 public class Sorting {
+    //se almacenara una copia de los numeros aleatorios por cada sort a utilizar para ordenar
+    private int[] listaSelection;
+    private int[] listaInsertion;
+    private int[] listaQuick;
+    private int[] listaRadix;
+    //linea que contendra los numeros del archivo de texto para despues ingresarlos en los arreglos
+    private String linea;
+    
+    public Sorting(){
+    //constructor de la clase Sorting
+    //cada arreglo contredra 3000 numeros
+    listaSelection= new int[3000];
+    listaInsertion= new int[3000];
+    listaQuick= new int[3000];
+    listaRadix= new int[3000];
+    }
     
     public int numerosRandom(){
+    //metodo que genera un numero aleatorio entre 1 y 10000
         int numero=(int)(Math.random()*(10000-1+1)+1);
         return numero;
     }
     
+    public void setString(String linea){
+      this.linea = linea;
+    }
+    
+    public void setListaSelection(){
+        int z=0;
+        for (int x=0; x<linea.length();x++){
+            if (linea.charAt(x)!=32){
+                listaSelection[z]=x;
+                z++;
+            }
+            else{
+                
+            }
+        }
+    }
+    
+    //este getter es solo para comprobar que el arreglo se haya guardado, HAY QUE BORRARLO en cuanto funcione
+    public int getListaSelection(int x){
+        return listaSelection[x];
+    }
     public void selectionSort(Comparable[] list){
     
     }
