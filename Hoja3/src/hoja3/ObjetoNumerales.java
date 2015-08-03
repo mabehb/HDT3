f99c0edd173b1278fa76cb1f0f4225c9c7847ea9
Daniela <Daniela@172.20.9.149>
2015-08-03 11:52:52 -0600

    Nuevo objeto

---
+++ Hoja3/src/hoja3/ObjetoNumerales.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hoja3;
+
+/**
+ *
+ * @author Daniela
+ */
+public class ObjetoNumerales implements Comparable {
+   
+    private int numero;
+    
+    public ObjetoNumerales(int valor){
+        numero = valor;
+    }
+    
+    public int compareTo(Object num){
+        
+        int resultado=0;
+        int numanterior = ((ObjetoNumerales)num).getNumero(); //obtiene el numero anterior a comparar
+        
+        if(numero>numanterior){ //compara numero actual con anterior
+            resultado = 1;
+        }
+        else{
+            resultado = 0;
+        }
+        
+        return resultado;
+    }
+    
+    public int getNumero(){
+        return numero;
+    }
+}
