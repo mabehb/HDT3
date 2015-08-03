69491950792265a288e62c61c81f879ec1ed6dc0
Daniela <Daniela@172.20.9.149>
2015-08-03 11:53:09 -0600

    Sorting modificado

--- Hoja3/src/hoja3/Sorting.java
+++ Hoja3/src/hoja3/Sorting.java
@@ -11,48 +11,11 @@ package hoja3;
  */
 public class Sorting {
     //se almacenara una copia de los numeros aleatorios por cada sort a utilizar para ordenar
-    private int[] selectionArray;
-    private int[] insertionArray;
-    private int[] quickArray;
-    private int[] radixArray;
-    
-    public Sorting(){
-    //constructor de la clase Sorting
-    //cada arreglo contredra 3000 numeros
-    selectionArray= new int[3000];
-    insertionArray= new int[3000];
-    quickArray= new int[3000];
-    radixArray= new int[3000];
-    }
     
     public int numerosRandom(){
     //metodo que genera un numero aleatorio entre 1 y 10000
         int numero=(int)(Math.random()*(10000-1+1)+1);
         return numero;
-    }
-    
-    public void setArreglos(int x, int z){
-    //setter para tener 4 copias de los numeros aleatorios
-        this.selectionArray[x]=z;
-        this.insertionArray[x]=z;
-        this.quickArray[x]=z;
-        this.radixArray[x]=z;
-    }
-    
-    public int getSelectionArray(int x){
-        return selectionArray[x];
-    }
-    
-    public int getInsertionArray(int x){
-        return insertionArray[x];
-    }
-    
-    public int getQuickArray(int x){
-        return quickArray[x];
-    }
-    
-    public int getRadixArray(int x){
-        return radixArray[x];
     }
     
     public void selectionSort(){
