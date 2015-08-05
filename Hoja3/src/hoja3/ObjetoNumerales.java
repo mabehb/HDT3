f37a292db7c3c2f8d442ea7eb2824df051dd90db
joserivera <joserivera@192.168.0.12>
2015-08-04 23:41:30 -0600

    Ya puse mi sort pero le falta el fucking compareTo .-. y agregué comentarios de JavaDoc

--- Hoja3/src/hoja3/ObjetoNumerales.java
+++ Hoja3/src/hoja3/ObjetoNumerales.java
@@ -4,20 +4,30 @@
  * and open the template in the editor.
  */
 
-package hoja3;
-
 /**
- *
- * @author Daniela
+ * Clase que implementa a la interfaz Comparable. En esta se generan los metodos
+ * que comparan numeros y generan objetos de arreglos.
+ * @author Belen Hernandez, Alejandro Rivera, Daniela Pocasangre, Juan Diego Benitez
  */
 public class ObjetoNumerales implements Comparable {
    
     private int numero;
     
+    /**
+     * Crea objetos de arreglos. Sirve para generar diferentes arreglos de tipo
+     * entero (int).
+     * @param valor De tipo entero. Contiene el valor del numero.
+     */
     public ObjetoNumerales(int valor){
         numero = valor;
     }
     
+    /**
+     * Metodo de comparacion de elementos de un arreglo. Compara dos elementos
+     * de un arreglo para saber si son mayores, menores o iguales.
+     * @param num Es un elemento de un arreglo.
+     * @return resultado: Contiene 1 si el numero 1 es mayor al 2, y 0 si no lo es.
+     */
     public int compareTo(Object num){
         
         int resultado=0;
@@ -34,6 +44,10 @@ public class ObjetoNumerales implements Comparable {
         return resultado;
     }
     
+    /**
+     * Metodo que retorna el numero.
+     * @return numero: Es un elemento del arreglo.
+     */
     public int getNumero(){
         return numero;
     }
