6c81b86db2c4468831753a0e2517a70a017994f3
joserivera <joserivera@192.168.0.12>
2015-08-05 00:07:54 -0600

    SOY FELIIIIZZZZ!!!!!! :D

--- Hoja3/src/hoja3/ObjetoNumerales.java
+++ Hoja3/src/hoja3/ObjetoNumerales.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Clase que implementa a la interfaz Comparable. En esta se generan los metodos
  * que comparan numeros y generan objetos de arreglos.
